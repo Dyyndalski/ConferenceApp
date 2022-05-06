7318d086a97a7fa2468d548b716e551d245dd778
Dyyndalski <szaur.kacper@gmail.com>
2022-05-06 21:05:03 +0200

    Created frames TODO service, repo, controller

---
+++ src/main/java/pl/szaur/conferenceapp/UserService.java
@@ -0,0 +1,14 @@
+package pl.szaur.conferenceapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+
+    //TODO
+}
